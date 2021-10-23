fe11d7a1672cad2453a6e4fe3edcc4e3262cac00
jaden <cep450@nyu.edu>
2021-10-23 13:48:29 -0400

    first commit
    this has all the LETTER KEYS but nothing else

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+
+    public AudioSource myAudioSource;
+    public float audioScale;
+
+    public GameObject childObj;
+    float startJiggle = 0.1f;
+    float chJiggle = 0.075f;
+    float colorJMult = 0.3f;
+
+    float r;
+    float g;
+    float b;
+
+/*
+    float lerpTime = 0f;
+    float lerpLength = 5f;
+    float lerpSizeMax = 1.5f;
+*/
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        //generate a color for this 
+        r = Random.Range(0f, 1f);   
+        g = Random.Range(0f, 1f);  
+        b = Random.Range(0f, 1f);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pressed(int num) {
+
+        //play sound, pitch shifted so the keyboard acts like a piano 
+        //A (num 10) is middle C 
+        ////"The general rule is to multiply the frequency by 1.05946^n, where n is the number of semitones you want to raise it."
+        //and myAudioSource.pitch is a multiplier- starts at 1, 0.5 is half, ect
+        //so, just set it to 1.05946^n
+        //myAudioSource.pitch = Mathf.Pow(1.05946f, num - 10); //this one goes each step. doesnt account for black keys
+        /*
+        0   G   -17
+        1   A   -15
+        2   B   -13
+        3   C   -12
+        4   D   -10
+        5   E   -8
+        6   F   -7
+        7   G   -5
+        8   A   -3
+        9   B   -1
+        10  C   0
+        11  D   2
+        12  E   4
+        13  F   5
+        14  G   7
+        15  A   9
+        16  B   11
+        1y  C   12
+        18  D   14
+        19  E   16
+        20  F   17
+        21  G   19
+        22  A   21
+        23  B   23
+        24  C   24
+        25  D   26
+        */  
+
+        //semitone multiplier
+                //TODO would optimize this out by having the num be preconverted since it's done by hand/as constants in the Keyboard class. but whatever for now
+        //stands for semitone multiplier
+        int stm = 0;
+        switch(num) {
+            case 0: stm = -17; break;
+            case 1: stm = -15; break;
+            case 2: stm = -13; break;
+            case 3: stm = -12; break;
+            case 4: stm = -10; break;
+            case 5: stm = -8; break;
+            case 6: stm = -7; break;
+            case 7: stm = -5; break;
+            case 8: stm = -3; break;
+            case 9: stm = -1; break;
+            case 10: stm = 0; break;
+            case 11: stm = 2; break;
+            case 12: stm = 4; break;
+            case 13: stm = 5; break;
+            case 14: stm = 7; break;
+            case 15: stm = 9; break;
+            case 16: stm = 11; break;
+            case 17: stm = 12; break;
+            case 18: stm = 14; break;
+            case 19: stm = 16; break;
+            case 20: stm = 17; break;
+            case 21: stm = 19; break;
+            case 22: stm = 21; break;
+            case 23: stm = 23; break;
+            case 24: stm = 24; break;
+            case 25: stm = 26; break;
+        }
+        myAudioSource.pitch = Mathf.Pow(1.05946f, stm);
+        myAudioSource.PlayOneShot(myAudioSource.clip, audioScale);
+
+        //add another flower
+        GameObject flowerchild = Instantiate(childObj, new Vector3(transform.position.x + jiggle(chJiggle), transform.position.y + jiggle(chJiggle), 0), Quaternion.identity);
+        flowerchild.transform.SetParent(transform);
+
+        //give it a color, based on the baseline one. 
+        float rr = (r + jiggle(chJiggle * colorJMult));
+        float gg = (g + jiggle(chJiggle * colorJMult));
+        float bb = (b + jiggle(chJiggle * colorJMult));
+        Color c = new Color(rr, gg, bb, 1);
+        flowerchild.GetComponent<SpriteRenderer>().color = c;
+
+
+        //grow children 
+        //increase scale by (jiggle by current jiggle) 
+        //could do this as a subroutine to animate it 
+        for(int i = transform.childCount; i > 0; i--) {
+            float j = Random.Range(0f, startJiggle);
+            Vector3 v = transform.GetChild(i - 1).localScale;
+            v = new Vector3(v.x + j, v.y + j, v.z);
+            transform.GetChild(i-1).localScale = v;
+        }
+
+        //TODO 
+        //gets bigger/does a little bounce/animates 
+
+        //jiggle is gonna increase each time 
+        chJiggle += startJiggle;
+
+    }
+
+        float jiggle(float jvalue) {
+        return Random.Range(-jvalue, jvalue);
+    }
+}
+
+
---
+++ Assets/Scripts/Keyboard.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour
+{
+
+/*
+    instantiate the keyboard garden
+        run through em, give em a little random offset jiggle 
+*/
+
+    public GameObject flowerPrefab;
+
+    Flower [] flowers = new Flower[26];
+
+    float xyJiggle = 0.25F;
+
+    float xyMult = 1.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //instantiate a bunch of flowers 
+        float x = -5.45f;
+        float y = 0.7525f;
+        for(int i = 0; i < 26; i++) {
+            
+            x++;
+            if(i == 10) {
+                y--;
+                x = -4.5f;
+            } else if(i == 19) {
+                y--;
+                x = -4f;
+            }
+
+            //TODO also give a random color
+            
+            flowers[i] = Instantiate(flowerPrefab, new Vector3(x * xyMult + Random.Range(-xyJiggle, xyJiggle), y * xyMult + Random.Range(-xyJiggle, xyJiggle), 0), Quaternion.identity).GetComponent(typeof(Flower)) as Flower;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when each key is pressed... 
+        //there has to be a better way to do this. 
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            flowers[0].Pressed(0);
+        }
+        if(Input.GetKeyDown(KeyCode.W)) {
+            flowers[1].Pressed(1);
+        }
+        if(Input.GetKeyDown(KeyCode.E)) {
+            flowers[2].Pressed(2);
+        }
+        if(Input.GetKeyDown(KeyCode.R)) {
+            flowers[3].Pressed(3);
+        }
+        if(Input.GetKeyDown(KeyCode.T)) {
+            flowers[4].Pressed(4);
+        }
+        if(Input.GetKeyDown(KeyCode.Y)) {
+            flowers[5].Pressed(5);
+        }
+        if(Input.GetKeyDown(KeyCode.U)) {
+            flowers[6].Pressed(6);
+        }
+        if(Input.GetKeyDown(KeyCode.I)) {
+            flowers[7].Pressed(7);
+        }
+        if(Input.GetKeyDown(KeyCode.O)) {
+            flowers[8].Pressed(8);
+        }
+        if(Input.GetKeyDown(KeyCode.P)) {
+            flowers[9].Pressed(9);
+        }
+        if(Input.GetKeyDown(KeyCode.A)) {
+            flowers[10].Pressed(10);
+        }
+        if(Input.GetKeyDown(KeyCode.S)) {
+            flowers[11].Pressed(11);
+        }
+        if(Input.GetKeyDown(KeyCode.D)) {
+            flowers[12].Pressed(12);
+        }
+        if(Input.GetKeyDown(KeyCode.F)) {
+            flowers[13].Pressed(13);
+        }
+        if(Input.GetKeyDown(KeyCode.G)) {
+            flowers[14].Pressed(14);
+        }
+        if(Input.GetKeyDown(KeyCode.H)) {
+            flowers[15].Pressed(15);
+        }
+        if(Input.GetKeyDown(KeyCode.J)) {
+            flowers[16].Pressed(16);
+        }
+        if(Input.GetKeyDown(KeyCode.K)) {
+            flowers[17].Pressed(17);
+        }
+        if(Input.GetKeyDown(KeyCode.L)) {
+            flowers[18].Pressed(18);
+        }
+        if(Input.GetKeyDown(KeyCode.Z)) {
+            flowers[19].Pressed(19);
+        }
+        if(Input.GetKeyDown(KeyCode.X)) {
+            flowers[20].Pressed(20);
+        }
+        if(Input.GetKeyDown(KeyCode.C)) {
+            flowers[21].Pressed(21);
+        }
+        if(Input.GetKeyDown(KeyCode.V)) {
+            flowers[22].Pressed(22);
+        }
+        if(Input.GetKeyDown(KeyCode.B)) {
+            flowers[23].Pressed(23);
+        }
+        if(Input.GetKeyDown(KeyCode.N)) {
+            flowers[24].Pressed(24);
+        }
+        if(Input.GetKeyDown(KeyCode.M)) {
+            flowers[25].Pressed(25);
+        }
+    }
+}
